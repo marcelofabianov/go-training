82b31059dabc0201b0a5e61b7cae64a35424e9ac
Marcelo Fabiano <marcelofabianov@gmail.com>
2023-05-01 17:44:53 -0300

    feat: hello world

--- main.go
+++ main.go
@@ -1,17 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	print("\nHello ", "World ", 2022, " Go!")
-	println("Hello", "World ", 2022, " Go!")
-
-	///////////////////lib fmt////////////////////////
-	fmt.Println("Hello", "World ", 2022, " Go!")
-
-	s := fmt.Sprintf("%s", "string-here")
-	fmt.Printf("%s%s", s, "\n")
-
-	f := fmt.Sprintf("%0.2f", 34.56756)
-	fmt.Println("float:", f)
+	println("Hello World")
 }
